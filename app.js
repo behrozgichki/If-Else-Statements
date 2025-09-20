76a6ef06526959389efb3b8cb8cd82e14cbd4f40
behrozgichki <behrozgichki9@gmail.com>
2025-09-21 00:00:49 +0500

    5 questions Completed

---
+++ app.js
@@ -0,0 +1,41 @@
+// var age = prompt("Enter your age")
+
+// if(age >= 18 ){
+//     alert("You are an adult")
+// }else {
+//    alert ("You are Minor");
+// }
+
+// var number = prompt("Enter a number:");
+
+// if(number > 0){
+//     console.log("Positive Number");
+// }else{
+//     console.log("Not Positive");
+// }
+
+// var number = prompt("Enter a Number")
+
+// if(number % 2 === 0){
+//     alert("Even Number")
+// }
+// else{
+//     alert("Odd Number")
+// }
+
+// var username = prompt("Enter your Username")
+
+// if( username === "admin"){
+//     alert("Welocme Admin")
+// }else{
+//     alert("Acsess denied")
+// }
+
+// var num1 = prompt("Enter the first number")
+// var num2 = prompt("Enter the second number")
+
+// if(num1 == num2){
+//     console.log("Both Numbers are same");
+// }else{
+//     console.log(" The numbers are different")
+// }
