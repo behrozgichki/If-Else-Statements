062ba313324898abdae39a43cc1bcc9c9516eb1b
hammad-sohail <hammadsohail733@gmail.com>
2025-09-21 12:37:39 +0500

    If else statements completed

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//Q1
+
 // var age = prompt("Enter your age")
 
 // if(age >= 18 ){
@@ -6,6 +8,8 @@
 //    alert ("You are Minor");
 // }
 
+//Q2
+
 // var number = prompt("Enter a number:");
 
 // if(number > 0){
@@ -13,6 +17,8 @@
 // }else{
 //     console.log("Not Positive");
 // }
+
+//Q3
 
 // var number = prompt("Enter a Number")
 
@@ -23,6 +29,8 @@
 //     alert("Odd Number")
 // }
 
+//Q4
+
 // var username = prompt("Enter your Username")
 
 // if( username === "admin"){
@@ -30,6 +38,8 @@
 // }else{
 //     alert("Acsess denied")
 // }
+
+//Q5
 
 // var num1 = prompt("Enter the first number")
 // var num2 = prompt("Enter the second number")
@@ -40,6 +50,8 @@
 //     console.log(" The numbers are different")
 // }
 
+//Q6
+
 // var num1 = prompt("Enter first number");
 // var num2 = prompt("Enter second number");
 
@@ -49,6 +61,8 @@
 //     alert("Second is larger");
 // }
 
+//Q7
+
 // var number = prompt("Enter a number")
 
 // if(number >= 100 ){
@@ -56,6 +70,8 @@
 // }else{
 //     alert("Within range")
 // }
+
+//Q8
 
 // var score = prompt("Enter your score");
 
@@ -65,6 +81,8 @@
 //     console.log("You Failed.");
 // }
 
+//Q9
+
 // var temperature = prompt("Enter the temperature.");
 
 // if(temperature > 30){
@@ -72,6 +90,8 @@
 // }else{
 //     alert("It's not hot")
 // }
+
+//Q10
 
 // var number = prompt("Enter a number");
 
@@ -81,6 +101,18 @@
 //     alert("Not zero.");
 // }
 
+//Q11
+
+// var age =prompt("Enter your age");
+
+// if(age >= "60"){
+//     console.log("You are a senior citizen");
+// }else{
+//     console.log("You are not a senior.");
+// }
+
+//Q12
+
 // var Username = prompt("Enter your name");
 
 // if(Username === "Alice"){
@@ -88,7 +120,9 @@
 // }else{
 //     alert("You are not Alice!")
 // }
- 
+
+//Q13
+
 // var password =prompt("Enter the password");
 
 // if(password === "12345"){
@@ -97,3 +131,72 @@
 //     console.log("Wrong Passowrd");
 // }
 
+//Q14
+
+// var time = prompt("Enter the current hour");
+
+// if(time <= "12"){
+//           alert("Good Morining!");
+// }else{
+//           alert("Good afternoon!");
+// }
+
+//Q15
+
+// var number = prompt("Enter a number");
+// if(number % 5 == "0"){
+//           console.log("Divisible by 5");
+// }else{
+//           console.log("Not divisible by 5");
+// }
+
+//Q16
+
+// var game = prompt("Want to play a game? (yes/no)");
+
+// if (game === "yes"){
+//   alert("Let's play!");
+// }else{
+//     alert("Maybe next time!");
+// }
+
+//Q17
+
+// var color = prompt("Enter a color");
+// if (color == "blue") {
+//   console.log("Cool choice!");
+// } else {
+//   console.log("Interesting color!");
+// }
+
+//Q18
+
+// var day = prompt("What's the day today");
+// if(day == "sunday"){
+//           alert("Weekend!");
+// }else{
+//           alert("Weekday");
+// }
+
+//Q19
+
+// var fruit = prompt("Enter a fruit");
+
+// if (fruit === "banana" || fruit === "apple") {
+//   alert("We have that!");
+// } else {
+//   alert("Sorry, out of stock.");
+// }
+
+//Q20
+
+// var start = prompt("Type start");
+
+// if(start === "start"){
+//           alert("Game started!");
+// }else{
+//  alert ("Waiting for input...")
+// }
+
+
+// Advanced JavaScript Conditional Logic Practice
