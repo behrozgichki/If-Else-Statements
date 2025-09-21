5d1165f8a4ff6dcb7d49348dcda88e7745ff7b87
behrozgichki <behrozgichki9@gmail.com>
2025-09-21 11:10:46 +0500

    13 questions completed

--- app.js
+++ app.js
@@ -39,3 +39,61 @@
 // }else{
 //     console.log(" The numbers are different")
 // }
+
+// var num1 = prompt("Enter first number");
+// var num2 = prompt("Enter second number");
+
+// if(num1 > num2){
+//     alert("First is larger");
+// }else if(num2 > num1){
+//     alert("Second is larger");
+// }
+
+// var number = prompt("Enter a number")
+
+// if(number >= 100 ){
+//     alert("Too big!");
+// }else{
+//     alert("Within range")
+// }
+
+// var score = prompt("Enter your score");
+
+// if(score >= 50){
+//     console.log("You Passed!");
+// }else{
+//     console.log("You Failed.");
+// }
+
+// var temperature = prompt("Enter the temperature.");
+
+// if(temperature > 30){
+//     alert("Too hot!");
+// }else{
+//     alert("It's not hot")
+// }
+
+// var number = prompt("Enter a number");
+
+// if(number == 0){
+//     alert("Zero");
+// }else{
+//     alert("Not zero.");
+// }
+
+// var Username = prompt("Enter your name");
+
+// if(Username === "Alice"){
+//     alert("Hi, Alice!")
+// }else{
+//     alert("You are not Alice!")
+// }
+ 
+// var password =prompt("Enter the password");
+
+// if(password === "12345"){
+//     console.log("Correct password");
+// }else{
+//     console.log("Wrong Passowrd");
+// }
+
